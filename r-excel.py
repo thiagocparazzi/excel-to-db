71bff802150375d5d2a28c90b766d5a41b841167
thiagocparazzi <thiagocparazzi@gmail.com>
2019-10-14 16:37:35 -0300

    Getting a field from collection

--- r-excel.py
+++ r-excel.py
@@ -6,14 +6,19 @@ import pandas as pd
 from pymongo import MongoClient
 
 #lendo uma planilha do excel com python
-df = pd.read_excel(r'D:\Dados\Downloads\contrato.xlsx')
+df = pd.read_excel(r'D:\Dados\Downloads\IncompletocontratoLOAABYZ.xlsx')
 
 #print(df)
 
 #conectando com o banco de dados MongoDB Atlas
-client = MongoClient(r'»»MONGODB URL OF YOUR CLUSTER««')
+client = MongoClient(r'mongodb+srv://nickinho:nickinho@cluster0-1azlf.mongodb.net/test?retryWrites=true&w=majority')
 db = client.cluster0
 
-collection = db['CFTS-LOOACPO']
+collection = db['CFTS-LOAABYZ']
 
 collection.insert_many(df.to_dict('records'))
+
+
+#transforma novamente em dataframe
+df = pd.DataFrame(list(collection.find({ }, { 'WORKNUMBER': '1'})))
+print(df)
